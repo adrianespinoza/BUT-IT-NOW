1b505c5f492dd01ab0fc2d3fa829437bec69069d
Adrian Espinoza <av.espinoza@hotmail.es>
2017-08-27 23:46:57 -0400

    Form created for inserting a new product to shopping cart

--- src/modules/shoppingCart/cart-dashboard/cart-dashboard.controller.js
+++ src/modules/shoppingCart/cart-dashboard/cart-dashboard.controller.js
@@ -1,14 +1,49 @@
 angular.module('shoppingCartApp.cartDashboard').controller('shoppingCart.CartDashboardController', [
   '$scope',
   'shoppingCartList',
+  'offeredProductsList',
   function (
       $scope,
-      shoppingCartList
+      shoppingCartList,
+      offeredProductsList
   ) {
     'use strict';
-    $scope.items = shoppingCartList;
+
+    var addProductToItemsList = function (item, items) {
+      var found = false;
+      for(var i = 0; i < items.length; i++) {
+        if (items[i].title === item.title) {
+          items[i] = item;
+          found = true;
+          break;
+        }
+      }
+      if (!found) {
+        items.push(item);
+      }
+    };
+
     $scope.remove = function (index) {
       $scope.items.splice(index, 1);
     };
+
+    $scope.onChangeOfferedProduct = function () {
+      $scope.offeredProductSelected.quantity = undefined;
+    };
+
+    $scope.addProduct = function (form) {
+      if (!form.$invalid) {
+        var item = angular.copy($scope.offeredProductSelected);
+        addProductToItemsList(item, $scope.items);
+        $scope.offeredProductSelected = undefined;
+      }
+    };
+
+    var init = function () {
+      $scope.items = shoppingCartList;
+      $scope.offeredProductSelected = undefined;
+      $scope.offeredProducts = offeredProductsList;
+    };
+    init();
   }
 ]);
--- test/spec/modules/shoppingCart/cart-dashboard/cart-dashboart.controller.spec.js
+++ test/spec/modules/shoppingCart/cart-dashboard/cart-dashboart.controller.spec.js
@@ -1,7 +1,8 @@
 describe('Tests for Cart Dashboard Controller: shoppingCart.CartDashboardController', function () {
     'use strict';
     var scope,
-        shoppingCartListMock;
+      shoppingCartListMock,
+      offeredProductsListMock;
 
     beforeEach(module('shoppingCartApp'));
     beforeEach(module('shoppingCartApp.cartDashboard'));
@@ -11,23 +12,53 @@ describe('Tests for Cart Dashboard Controller: shoppingCart.CartDashboardControl
         '$rootScope',
         '$controller',
         function (
-            $rootScope,
-            $controller
+          $rootScope,
+          $controller
         ) {
             scope = $rootScope.$new();
             shoppingCartListMock = [
                 {title: 'TV', quantity: 1, price: 500},
                 {title: 'Radio', quantity: 1, price: 80}
             ];
+            offeredProductsListMock = [
+                {title: 'TV', price: 500},
+                {title: 'Radio', price: 80},
+                {title: 'Microwave', price: 150},
+                {title: 'ChromeCast', price: 70}
+            ];
             $controller('shoppingCart.CartDashboardController', {
                 $scope: scope,
-                shoppingCartList: shoppingCartListMock
+                shoppingCartList: shoppingCartListMock,
+                offeredProductsList: offeredProductsListMock
             });
         }
     ]));
 
     it('should be set the items var in controller', function () {
         var items = scope.items;
-        expect(items.length).toBeGreaterThan(0);;
+        expect(items.length).toBeGreaterThan(0);
+    });
+
+    it('should remove an item', function () {
+        var items = scope.items;
+        expect(items.length).toBe(2);
+        scope.remove(1);
+        expect(items.length).toBe(1);
+    });
+
+    it('should add a different item', function () {
+        var items = scope.items;
+        expect(items.length).toBe(2);
+        scope.offeredProductSelected = {title: 'Microwave', price: 150};
+        scope.addProduct({$invalid: false});
+        expect(items.length).toBe(3);
+    });
+
+    it('should add an existing item', function () {
+        var items = scope.items;
+        expect(items.length).toBe(2);
+        scope.offeredProductSelected = {title: 'TV', price: 500};
+        scope.addProduct({$invalid: false});
+        expect(items.length).toBe(2);
     });
 });
